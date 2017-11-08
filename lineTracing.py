edfefed4c85ba62ff33665cb554a6bc91a9265cb
Younghoon Kim <the_basic_@kookmin.ac.kr>
2017-11-08 16:11:10 +0900

    Edit code

--- lineTracing.py
+++ lineTracing.py
@@ -15,7 +15,7 @@ def lineTracing():
     while True:
         output = get_DBACE()
         OTD, OTB, OTA, OTC, OTE = output[0], output[1] ,output[2], output[3], output[4]
-
+        print(get_DBACE())
 
         # if OTD == 1 and OTE == 1 and (OTB == 0 or OTA == 0 or OTC == 0):  # Case1
         #     go_forward_diff(50, 50)
@@ -26,34 +26,34 @@ def lineTracing():
         # else:
         #     go_forward_diff(0, 0)
 
-        if OTD == 1 and OTB == 1 and OTA == 1 and OTC == 1 and OTE == 1: # 라인 이탈 (All White)
-            go_forward_diff(0, 0)
-        elif OTD == 0 and OTB == 0 and OTA ==0 and OTC == 0 and OTE == 0: # 감지 불가 (All Black)
-            go_forward_diff(0, 0)
-        elif OTD == 1 and OTB == 1 and OTA == 0 and OTC == 1 and OTE == 1:
-            go_forward_diff(20, 20)
-        elif OTD == 0 and OTB == 1 and OTA == 1 and OTC == 1 and OTE == 1:
-            go_forward_diff(5, 20)
-        elif OTD == 0 and OTB == 0 and OTA == 1 and OTC == 1 and OTE == 1:
-            go_forward_diff(10, 20)
-        elif OTD == 0 and OTB == 0 and OTA == 0 and OTC == 1 and OTE == 1:
-            go_forward_diff(15, 20)
-        elif OTD == 1 and OTB == 0 and OTA == 0 and OTC == 1 and OTE == 1:
-            go_forward_diff(17, 20)
-        elif OTD == 1 and OTB == 1 and OTA == 0 and OTC == 0 and OTE == 1:
-            go_forward_diff(20, 17)
-        elif OTD == 1 and OTB == 1 and OTA == 0 and OTC == 0 and OTE == 0:
-            go_forward_diff(20, 15)
-        elif OTD == 1 and OTB == 1 and OTA == 1 and OTC == 0 and OTE == 0:
-            go_forward_diff(20, 10)
-        elif OTD == 1 and OTB == 1 and OTA == 1 and OTC == 1 and OTE == 0:
-            go_forward_diff(20, 5)
-        elif OTD == 1 and OTB == 1 and OTA == 1 and OTC == 0 and OTE == 1:
-            go_forward_diff(20, 15)
-        elif OTD == 1 and OTB == 0 and OTA == 1 and OTC == 1 and OTE == 1:
-            go_forward_diff(15, 20)
-        else:
-            go_forward_diff(0, 0)
+        # if OTD == 1 and OTB == 1 and OTA == 1 and OTC == 1 and OTE == 1: # 라인 이탈 (All White)
+        #     go_forward_diff(0, 0)
+        # elif OTD == 0 and OTB == 0 and OTA ==0 and OTC == 0 and OTE == 0: # 감지 불가 (All Black)
+        #     go_forward_diff(0, 0)
+        # elif OTD == 1 and OTB == 1 and OTA == 0 and OTC == 1 and OTE == 1:
+        #     go_forward_diff(20, 20)
+        # elif OTD == 0 and OTB == 1 and OTA == 1 and OTC == 1 and OTE == 1:
+        #     go_forward_diff(5, 20)
+        # elif OTD == 0 and OTB == 0 and OTA == 1 and OTC == 1 and OTE == 1:
+        #     go_forward_diff(10, 20)
+        # elif OTD == 0 and OTB == 0 and OTA == 0 and OTC == 1 and OTE == 1:
+        #     go_forward_diff(15, 20)
+        # elif OTD == 1 and OTB == 0 and OTA == 0 and OTC == 1 and OTE == 1:
+        #     go_forward_diff(17, 20)
+        # elif OTD == 1 and OTB == 1 and OTA == 0 and OTC == 0 and OTE == 1:
+        #     go_forward_diff(20, 17)
+        # elif OTD == 1 and OTB == 1 and OTA == 0 and OTC == 0 and OTE == 0:
+        #     go_forward_diff(20, 15)
+        # elif OTD == 1 and OTB == 1 and OTA == 1 and OTC == 0 and OTE == 0:
+        #     go_forward_diff(20, 10)
+        # elif OTD == 1 and OTB == 1 and OTA == 1 and OTC == 1 and OTE == 0:
+        #     go_forward_diff(20, 5)
+        # elif OTD == 1 and OTB == 1 and OTA == 1 and OTC == 0 and OTE == 1:
+        #     go_forward_diff(20, 15)
+        # elif OTD == 1 and OTB == 0 and OTA == 1 and OTC == 1 and OTE == 1:
+        #     go_forward_diff(15, 20)
+        # else:
+        #     go_forward_diff(0, 0)
 
 
 if __name__ == '__main__':
