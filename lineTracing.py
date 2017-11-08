a870cdb5dea0f0a99cc5138251728432342f1f40
Younghoon Kim <the_basic_@kookmin.ac.kr>
2017-11-08 15:55:55 +0900

    Edit code

--- lineTracing.py
+++ lineTracing.py
@@ -30,27 +30,27 @@ def lineTracing():
         elif OTD == 0 and OTB == 0 and OTA ==0 and OTC == 0 and OTE == 0: # 감지 불가 (All Black)
             go_forward_diff(0, 0)
         elif OTD == 1 and OTB == 1 and OTA == 0 and OTC == 1 and OTE == 1:
-            go_forward_diff(50, 50)
+            go_forward_diff(20, 20)
         elif OTD == 0 and OTB == 1 and OTA == 1 and OTC == 1 and OTE == 1:
-            go_forward_diff(20, 50)
+            go_forward_diff(5, 20)
         elif OTD == 0 and OTB == 0 and OTA == 1 and OTC == 1 and OTE == 1:
-            go_forward_diff(30, 50)
+            go_forward_diff(10, 20)
         elif OTD == 0 and OTB == 0 and OTA == 0 and OTC == 1 and OTE == 1:
-            go_forward_diff(40, 50)
+            go_forward_diff(15, 20)
         elif OTD == 1 and OTB == 0 and OTA == 0 and OTC == 1 and OTE == 1:
-            go_forward_diff(50, 45)
+            go_forward_diff(17, 20)
         elif OTD == 1 and OTB == 1 and OTA == 0 and OTC == 0 and OTE == 1:
-            go_forward_diff(50, 45)
+            go_forward_diff(20, 17)
         elif OTD == 1 and OTB == 1 and OTA == 0 and OTC == 0 and OTE == 0:
-            go_forward_diff(50, 40)
+            go_forward_diff(20, 15)
         elif OTD == 1 and OTB == 1 and OTA == 1 and OTC == 0 and OTE == 0:
-            go_forward_diff(50, 30)
+            go_forward_diff(20, 10)
         elif OTD == 1 and OTB == 1 and OTA == 1 and OTC == 1 and OTE == 0:
-            go_forward_diff(50, 20)
+            go_forward_diff(20, 5)
         elif OTD == 1 and OTB == 1 and OTA == 1 and OTC == 0 and OTE == 1:
-            go_forward_diff(45, 50)
+            go_forward_diff(20, 15)
         elif OTD == 1 and OTB == 0 and OTA == 1 and OTC == 1 and OTE == 1:
-            go_forward_diff(50, 45)
+            go_forward_diff(15, 20)
         else:
             go_forward_diff(0, 0)
 
